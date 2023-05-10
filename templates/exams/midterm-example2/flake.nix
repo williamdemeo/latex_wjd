dba6b590ad826fa25850da1da51da4e6d527f17e
William DeMeo <williamdemeo@gmail.com>
2023-05-10 16:37:15 -0400

    reorg

---
+++ templates/exams/midterm-example2/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "DS 644 Midterm Exam";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-medium
+            latex-bin
+            latexmk
+            exam
+            geometry
+            amsmath
+            amstex
+            amscls
+            mathtools
+            comment
+            mathalpha
+            bbold
+            newunicodechar
+            background
+            everypage;
+      };
+    in rec {
+      packages = {
+        document = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "ds644-midterm-exam";
+          src = self;
+          buildInputs = [ pkgs.coreutils tex ];
+          phases = ["unpackPhase" "buildPhase" "installPhase"];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex -f \
+              midterm.tex
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex -f \
+              midterm-SOLUTIONS.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp midterm.pdf midterm-SOLUTIONS.pdf $out/
+          '';
+        };
+      };
+      defaultPackage = packages.document;
+    });
+}
